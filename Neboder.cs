61aac9ebc7b49e2fb5138a6573cfc7d1a509b4c9
dinooo <145918377+dindooo@users.noreply.github.com>
2023-12-19 10:46:11 +0100

    Add files via upload

---
+++ Neboder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neboderOcjena
+{
+    internal class Neboder
+    {
+       
+        float visina;
+        int brojKatova;
+
+        public void setVisina(float visina)
+        {
+            this.visina = visina;
+        }
+        public float getVisina()
+        {
+            return this.visina;
+        }
+        public void setBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+        public int getBrojKatova()
+        {
+            return this.brojKatova;
+        }
+
+        public Neboder(float visina, int brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+        }
+
+        public override string ToString()
+        {
+            string ispis = "Visina " + this.getVisina() + " Broj katova " + this.getBrojKatova() + " Prosjek je " + this.getVisina()/this.getBrojKatova();
+            return ispis;
+        }
+    }
+
+}
